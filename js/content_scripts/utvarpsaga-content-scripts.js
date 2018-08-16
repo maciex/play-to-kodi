4cfac7224d55a306ca5a0b72a99a5f2a410a48c2
Jóhann Berentsson <jberentsson@gmail.com>
2018-08-16 06:42:29 +0000

    fix

--- js/content_scripts/utvarpsaga-content-scripts.js
+++ js/content_scripts/utvarpsaga-content-scripts.js
@@ -1,7 +1,11 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if ("getUtvarpSagaUrl" == request.action) {
-            var link = $('html').html().match("(https|http).*?\\.mp3")[0];
+            try {
+                var link = $('html').find('source').attr('src');
+            }catch(e){
+                var link = $('html').html().match("(https|http).*?\\.mp3")[0];
+            }
             if (link) {
                 sendResponse({url: link.split('?')[0]});
             }
