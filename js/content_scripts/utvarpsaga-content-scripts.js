7f46aff25ee863099a2f08947c43f8f05a49b05f
Jóhann Berentsson <jberentsson@gmail.com>
2018-08-16 06:57:27 +0000

    refactor

--- js/content_scripts/utvarpsaga-content-scripts.js
+++ js/content_scripts/utvarpsaga-content-scripts.js
@@ -1,13 +1,33 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        if ("getUtvarpSagaUrl" == request.action) {
+        var findAudioLink = function(){
             try {
-                var link = $('html').find('source').attr('src');
+                return $('html').html().match("(https|http).*?\\.mp3")[0];
             }catch(e){
-                var link = $('html').html().match("(https|http).*?\\.mp3")[0];
+                return null;
             }
+        }
+
+        var findStreamLink = function(){
+            try {
+                return $('html').find('source').attr('src');
+            }catch(e){
+                return null;
+            }
+        }
+
+        var play_file = function(link){
+            sendResponse({url: link.split('?')[0]});
+        }
+
+        if ("getUtvarpSagaUrl" == request.action) {
+            var link = findStreamLink();
             if (link) {
-                sendResponse({url: link.split('?')[0]});
+                console.log('Stream');
+                play_file(link);
+            }else{
+                console.log('File');
+                play_file(findAudioLink());
             }
         }
     }
