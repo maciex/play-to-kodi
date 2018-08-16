2b76d31980122139e96025d602256b6369b4b554
Jóhann Berentsson <jberentsson@gmail.com>
2018-08-16 05:32:03 +0000

    misc

--- js/content_scripts/visiris-content-scripts.js
+++ js/content_scripts/visiris-content-scripts.js
@@ -1,18 +1,76 @@
+
+
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        try {
-
-            var videoLink = $('html').html().match("\/hls\/.*?\\.m3u8")[0];
-            var hostLink = $('html').html().match("Host: '.*?\\',")[0].slice(7, -2);
-            console.log(videoLink);
-            console.log(hostLink);
-        }catch(e){
-            console.log(e);
-        }
-
         if ("getVisirIsUrl" == request.action) {
+            var getVideoLink = function(html){
+                try {
+                    var videoLink = html.match("\/hls\/.*?\\.m3u8")[0];
+                    console.log(videoLink);
+                    return videoLink;
+                }catch(e){
+                    console.log(e);
+                    return None;
+                }
+            }
+            
+            var getHostLink = function(html){
+                try {
+                    var hostLink = html.match("Host: '.*?\\',")[0].slice(7, -2);
+                    console.log(hostLink);
+                    return hostLink;
+                }catch(e){
+                    console.log(e);
+                    return None;
+                }
+            }
+            
+            var getIframeSrc = function(html){
+                try {
+                    var iframeUrl = html.find('figure').find('iframe').attr('src');
+                    var url = 'http://www.visir.is' + iframeUrl;
+
+                    $.get(url, function(data, status){
+                        //alert("Data: " + data + "\nStatus: " + status);
+                        var uri = data.match("\/hls\/.*?\\.m3u8")[0]
+                        var host = data.match("Host: '.*?\\',")[0].slice(7, -2);
+
+                        return host + uri
+                    }).then(function(url){
+                        sendResponse({url: 'http://' + url});
+                    });
+                }catch(e){
+                    console.log(e);
+                    return None;
+                }
+            }
+
+            try{
+                var videoLink = getVideoLink($('html').html());
+
+            }catch(e){
+
+            }
+
+            try{
+                var hostLink = getHostLink($('html').html());
+
+            }catch(e){
+                
+            }
+
+            try{
+
+                var IframeSrc = getIframeSrc($('html'));
+            }catch(e){
+                
+            }
+
             if (videoLink) {
-                sendResponse({url: 'https://' + hostLink+videoLink});
+                sendResponse({url: 'http://' + hostLink+videoLink});
+            }else if(IframeSrc){
+                console.log(IframeSrc);
+                sendResponse({url: 'http://' + IframeSrc});
             }
         }
     }
