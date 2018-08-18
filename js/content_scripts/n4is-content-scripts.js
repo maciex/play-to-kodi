33a1dbadaee993921763c062c6aa750af3c529cf
Jóhann Berentsson <jberentsson@gmail.com>
2018-08-18 01:33:57 +0000

    n4 fix

--- js/content_scripts/n4is-content-scripts.js
+++ js/content_scripts/n4is-content-scripts.js
@@ -1,7 +1,7 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if ("getN4IsUrl" == request.action) {
-            var videoLink = $('html').find('source').attr('src');
+            var videoLink = $('html').find('iframe').attr('src');
             if (videoLink) {
                 sendResponse({url: videoLink});
             }
